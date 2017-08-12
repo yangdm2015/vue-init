4190c51bbbd760297d69a7e99ea5979b9003c311
yangdm2015 <yangshan503@126.com>
2017-08-12 11:03:23 +0800

    first commit

---
+++ src/components/d1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d1">
+    d1
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: '',
+        data() {
+            return {}
+        },
+        created() {
+
+        },
+        methods: {},
+        components: {}
+    }
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+</style>
